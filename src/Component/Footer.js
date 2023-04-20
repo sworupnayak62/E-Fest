dea21b04347adcbb848f2b9bf25ffe383ffc2fa8
SWAUGAT-BEURA <graswaugatbeura@gmail.com>
2023-04-20 12:39:23 +0530

    loading, blue  color change ,gallery,team update

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../css/footer.css';
 import { FaFacebook } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa';
+import { BiLinkAlt } from 'react-icons/bi';
 import { FaYoutube } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
@@ -15,7 +15,10 @@ export const Footer = () => {
       <div className="top_header">
         <section>
           <span><ImLocation className="fa fa-map-marker" /></span>
-          <span>Street, full address, state/province, country, pincode</span>
+          <span>Silicon Institute of Technology,
+            Silicon Hills, Patia,
+            Bhubaneswar – 751024
+            Odisha, India.</span>
         </section>
         <section>
           <span><BsFillTelephoneFill className="fa fa-phone" /></span>
@@ -29,23 +32,22 @@ export const Footer = () => {
       <span class="border-shape"></span>
       <div class="bottom_content">
         <section className='social-links-footer'>
-          <span className="fa fa-envelope"><FaFacebook /></span>
-          <span className="fa fa-envelope"><FaInstagram /></span>
-          <span className="fa fa-envelope"><FaTwitter /></span>
-          <span className="fa fa-envelope"><FaLinkedin /></span>
-
-
+          <a href="https://www.facebook.com/entrepreneurship.cell.1"><span className="fa fa-envelope"><FaFacebook /></span></a>
+          <a href="https://www.instagram.com/sit.edcell/"><span className="fa fa-envelope"><FaInstagram /></span></a>
+          <a href="https://silicon.ac.in/entrepreneurship-dev-cell/"><span className="fa fa-envelope"><BiLinkAlt /></span></a>
+          <a href="https://www.linkedin.com/company/entrepreneurshipdevelopmentcellsitbbsr/about/"><span className="fa fa-envelope"><FaLinkedin /></span></a>
         </section>
+
         <section className='footer-links'>
-          <a href="#">Home</a>
-          <a href="#">Faq</a>
-          <a href="#">Gallery</a>
-          <a href="#">Events</a>
-          <a href="#">Teams</a>
+          <a href="/">Home</a>
+          <a href="#faq">Faq</a>
+          <a href="#gallery">Gallery</a>
+          <a href="#event">Events</a>
+          <a href="/team">Teams</a>
         </section>
       </div>
       <div className="copyright">
-        Copyright © 2023 websitename - All rights reserved
+        Copyright © 2023 SIT,Entrepreneurship Development Cell - All rights reserved
       </div>
     </footer >
   )
